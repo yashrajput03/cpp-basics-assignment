f37a5b9475f676b19dfa77dff22d3c0d50242534
yashrajput03 <132838733+yashrajput03@users.noreply.github.com>
2024-02-19 22:57:11 +0530

    Solved assignment files are added

---
+++ Circumferenceofcircle.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int r =10;
+    float circumference = 2*3.14*r;
+    cout<<"circumference of circle =" << circumference; 
+}
---
+++ print10.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 5;
+    int b = 5;
+    int sum = a+b;
+    cout<<sum<<endl;
+}
